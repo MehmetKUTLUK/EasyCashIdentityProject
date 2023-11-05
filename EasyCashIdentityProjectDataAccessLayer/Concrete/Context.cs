ebd0615a7aceb578b597ce718bee258077c42ad5
MONSTER <MONSTER@MEHMETKUTLUK>
2023-11-05 03:00:44 +0300

    identity

---
+++ EasyCashIdentityProjectDataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EasyCashIdentityProjectEntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProjectDataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext<AppUser,AppRole,Guid>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;initial catalog=EasyCashDb;integrated Security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+
+    }
+}
